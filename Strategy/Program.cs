38230c5ccd7954d6a920e29fb9ba81067650f4e8
vasif <vasifbabazade2004@gmail.com>
2022-12-02 21:18:00 +0400

    Add project files.

---
+++ Strategy/Program.cs
@@ -0,0 +1,61 @@
+﻿// The Strategy declares an interface that is common to all supported algorithms. The Context object uses
+// this interface to call the algorithm defined by a ConcreteStrategy
+public interface ICompression
+{
+    void CompressFolder(string compressedArchiveFileName);
+}
+
+// Each concrete strategy by which we will compress a file item
+// must be implementing the method CompressFolder of ICompression interface.
+// Let’s create two concrete strategy classes as per our business requirement.
+
+public class RarCompression : ICompression
+{
+    public void CompressFolder(string compressedArchiveFileName)
+    {
+        Console.WriteLine("Folder is compressed using Rar approach: '" + compressedArchiveFileName
+             + ".rar' file is created");
+    }
+}
+
+
+public class ZipCompression : ICompression
+{
+    public void CompressFolder(string compressedArchiveFileName)
+    {
+        Console.WriteLine("Folder is compressed using zip approach: '" + compressedArchiveFileName
+             + ".zip' file is created");
+    }
+}
+
+//  This context class contains a property that is used to hold the reference of a Strategy object.
+//  This property will be set at run-time by the client according to the algorithm that is required.
+public class CompressionContext
+{
+    private ICompression Compression;
+
+    public CompressionContext(ICompression Compression)
+    {
+        this.Compression = Compression;
+    }
+    public void SetStrategy(ICompression Compression)
+    {
+        this.Compression = Compression;
+    }
+    public void CreateArchive(string compressedArchiveFileName)
+    {
+        Compression.CompressFolder(compressedArchiveFileName);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CompressionContext ctx = new CompressionContext(new ZipCompression());
+        ctx.CreateArchive("DotNetDesignPattern");
+        ctx.SetStrategy(new RarCompression());
+        ctx.CreateArchive("DotNetDesignPattern");
+        Console.Read();
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,81 @@
+﻿//  This Abstract Class defines a set of abstract methods that need to be implemented by the Concrete sub Classes.
+//  But if you want you can also provide a default implementation for the above methods and the child class can optionally override the implementation.
+//  It also provides one concrete method (i.e. template method) which defines the order in which those abstract operations occur. 
+public abstract class HouseTemplate
+{
+    // Template method defines the sequence for building a house
+    public void BuildHouse()
+    {
+        BuildFoundation();
+        BuildPillars();
+        BuildWalls();
+        BuildWindows();
+        Console.WriteLine("House is built");
+    }
+    // Methods to be implemented by subclasses
+    protected abstract void BuildFoundation();
+    protected abstract void BuildPillars();
+    protected abstract void BuildWalls();
+    protected abstract void BuildWindows();
+}
+
+// The Concrete Class implements the operations defined by the Abstract Class.
+// As we are going to create two types of house i.e. Concrete and Wooden
+// so we are going to create two concrete classes by implementing the abstract HouseTemplate class.
+
+public class ConcreteHouse : HouseTemplate
+{
+    protected override void BuildFoundation()
+    {
+        Console.WriteLine("Building foundation with cement, iron rods and sand");
+    }
+    protected override void BuildPillars()
+    {
+        Console.WriteLine("Building Concrete Pillars with Cement and Sand");
+    }
+    protected override void BuildWalls()
+    {
+        Console.WriteLine("Building Concrete Walls");
+    }
+    protected override void BuildWindows()
+    {
+        Console.WriteLine("Building Concrete Windows");
+    }
+}
+
+public class WoodenHouse : HouseTemplate
+{
+    protected override void BuildFoundation()
+    {
+        Console.WriteLine("Building foundation with cement, iron rods, wood and sand");
+    }
+    protected override void BuildPillars()
+    {
+        Console.WriteLine("Building wood Pillars with wood coating");
+    }
+    protected override void BuildWalls()
+    {
+        Console.WriteLine("Building Wood Walls");
+    }
+    protected override void BuildWindows()
+    {
+        Console.WriteLine("Building Wood Windows");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Build a Concrete House\n");
+        HouseTemplate houseTemplate = new ConcreteHouse();
+        // call the template method
+        houseTemplate.BuildHouse();
+        Console.WriteLine();
+        Console.WriteLine("Build a Wooden House\n");
+        houseTemplate = new WoodenHouse();
+        // call the template method
+        houseTemplate.BuildHouse();
+        Console.Read();
+    }
+}
